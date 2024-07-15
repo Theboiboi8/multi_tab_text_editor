73671ba24f2ee1c59b8dd199bca19835c82f0efa
Theboiboi8 <kamentanchev@gmail.com>
2024-07-15 14:17:25 +0300

    Theme colors

---
+++ src/editor_theme/colors.rs
@@ -0,0 +1,16 @@
+#![allow(clippy::unreadable_literal)]
+
+pub(in crate::editor_theme) const BACKGROUND_COLOR: usize = 0x141414;
+pub(in crate::editor_theme) const FOREGROUND_COLOR: usize = 0xb0b0b0;
+pub(in crate::editor_theme) const TEXT_COLOR: usize = 0x757575;
+pub(in crate::editor_theme) const SELECTION_BACKGROUND_COLOR: usize = 0x323232;
+pub(in crate::editor_theme) const SELECTION_FOREGROUND_COLOR: usize = 0xd7d7d7;
+pub(in crate::editor_theme) const BUTTON_COLOR: usize = 0x1c1c1c;
+pub(in crate::editor_theme) const SECONDARY_BACKGROUND_COLOR: usize = BACKGROUND_COLOR;
+pub(in crate::editor_theme) const DISABLED_COLOR: usize = 0x5a5a5a;
+pub(in crate::editor_theme) const CONTRAST_COLOR: usize = 0x101010;
+pub(in crate::editor_theme) const TABLE_SELECTION_COLOR: usize = 0x292929;
+pub(in crate::editor_theme) const NOTIFICATION_COLOR: usize = 0x111111;
+pub(in crate::editor_theme) const ACCENT_COLOR: usize = 0x670000;
+pub(in crate::editor_theme) const EXCLUDED_COLOR: usize = 0x3e1818;
+pub(in crate::editor_theme) const SELECTION_TRANSPARENT_BACKGROUND_COLOR: usize = 0x191919;
